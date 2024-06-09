b657f43aa7115849cda535e50d51909f066ae889
Franklin Orellana <franklin0994324757@gmail.com>
2024-06-09 01:01:39 -0500

    Commit 1

--- AWSProyectoUsuario/src/main/java/com/ista/ejercicio/factory/UsuarioFactory.java
+++
@@ -1,9 +0,0 @@
-package com.ista.ejercicio.factory;
-
-import com.ista.ejercicio.entity.Usuario;
-
-public interface UsuarioFactory {
-	
-	 Usuario crearUsuario(String nombre, String clave, String email, Boolean estado);
-
-}
